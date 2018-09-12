cf30ea6700cf44e792f5d2ab7ee5135acace6ee1
seni1 <seni@bestpick.ru>
2018-09-12 15:23:48 +0300

    Problems with populateUI method

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +16,25 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    //TODO: Why declare as public and not private?
+    public ImageView sandwichIv;
+    public TextView descriptionTv;
+    public TextView originTv;
+    public TextView alsoKnownAs;
+    public TextView ingredientsTv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+       sandwichIv = findViewById(R.id.image_iv);
+       descriptionTv = findViewById(R.id.description_tv);
+       originTv = findViewById(R.id.origin_tv);
+       alsoKnownAs = findViewById(R.id.also_known_tv);
+       ingredientsTv = findViewById(R.id.ingredients_tv);
+
+
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,10 +57,11 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        //TODO: Why is this method here, and not in the "populateUI" method?
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
-                .into(ingredientsIv);
+                .into(sandwichIv);
 
         setTitle(sandwich.getMainName());
     }
@@ -56,7 +71,19 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    //TODO: Why only 2 attributes: description and origin get populated via this method?
+    private void populateUI(Sandwich sandwich) {
+        descriptionTv.setText(sandwich.getDescription());
+        originTv.setText(sandwich.getPlaceOfOrigin());
+        //TODO:  Don't understand this "for" loops: I see that Arrays get set through loops, while Strings through setText - WHY!
+
+        for (int i = 0; i < sandwich.getAlsoKnownAs().size(); i ++) {
+            alsoKnownAs.append(sandwich.getAlsoKnownAs().get(i));
+        }
+
+        for (int i = 0; i < sandwich.getIngredients().size(); i ++) {
+            ingredientsTv.append(sandwich.getIngredients().get(i) + "\n");
+        }
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,70 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class JsonUtils {
 
+    //TODO: What's the variable "VAL_NAME" for?
+    public static final String VAL_NAME = "name";
+    public static final String VAL_MAIN_NAME = "mainName";
+    public static final String VAL_AKA = "alsoKnownAs";
+    public static final String VAL_DESCRIPTION = "description";
+    public static final String VAL_ORIGIN_PLACE = "placeOfOrigin";
+    public static final String VAL_INGREDIENTS = "ingredients";
+    public static final String VAL_IMAGE = "image";
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        //TODO: Why are we declaring the Sandwich object?
+        Sandwich sandwich = new Sandwich();
+
+        try {
+            //TODO: What's the rootObject, and why are we declaring it?
+            JSONObject rootObject = new JSONObject(json);
+
+            //Here creating a JSON "nameObject"
+            JSONObject nameObject = rootObject.getJSONObject(VAL_NAME);
+
+            //Here we're calling "setMainName" method from Sandwich.java class
+            // on the sandwich object just created.
+            sandwich.setMainName(nameObject.getString(VAL_MAIN_NAME));
+            //Here we're calling "setAlsoKnownAs" method with attributes: "getJsonArrayList",
+            // that had to be declared as a method bellow.
+            //TODO: Why we use an Array here as opposed to String?
+            sandwich.setAlsoKnownAs(getJsonArrayAsList(rootObject.getJSONArray(VAL_AKA)));
+
+            sandwich.setPlaceOfOrigin(rootObject.getString(VAL_ORIGIN_PLACE));
+
+            sandwich.setDescription(rootObject.getString(VAL_DESCRIPTION));
+
+            sandwich.setPlaceOfOrigin(rootObject.getString(VAL_ORIGIN_PLACE));
+
+            sandwich.setImage(rootObject.getString(VAL_IMAGE));
+
+            sandwich.setIngredients(getJsonArrayAsList(rootObject.getJSONArray(VAL_INGREDIENTS)));
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return sandwich;
+
     }
+
+    public static ArrayList<String> getJsonArrayAsList(JSONArray array) {
+        ArrayList<String > list = new ArrayList<>();
+        try {
+            for (int i = 0; i < array.length(); i++) {
+                list.add(array.getString(i));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
 }
